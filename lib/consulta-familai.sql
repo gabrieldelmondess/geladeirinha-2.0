508f8ad55b9b9b0efd9fefc4b551cb1f03dee14a
Gabriel Delmondes <105651767+gabrieldelmondess@users.noreply.github.com>
2024-06-16 22:28:18 -0300

    Update consulta-familai.sql

--- lib/consulta-familai.sql
+++ lib/consulta-familai.sql
@@ -1,7 +1,15 @@
 SELECT
   f."nomeFamilia" AS "Nome da Família",
-  p.nome AS "Nome do Participante",
+  CONCAT(
+    CASE WHEN NOT EXISTS (
+      SELECT 1 FROM perfil AS p2
+      JOIN familia AS f2 ON p2."familiaID" = f2.id
+      WHERE f2."nomeFamilia" = f."nomeFamilia" AND p2.id < p.id
+    ) THEN '*' ELSE '' END,
+    p.nome
+  ) AS "Nome do Participante",
   p.relacao AS "Relação na Família"
 FROM perfil AS p
-JOIN familia AS f ON p."familiaID" = f.id  
+JOIN familia AS f ON p."familiaID" = f.id
 ORDER BY f."nomeFamilia", p.relacao;
+
